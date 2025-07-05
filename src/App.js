ecc885a30f558ce3b1599cc7c2a2ff911a19a6d4
Chuong Cao <chuongb2105536@student.ctu.edu.vn>
2025-07-05 17:37:25 +0700

    Form's State va Validation

--- src/App.js
+++ src/App.js
@@ -1,43 +1,58 @@
 import './App.scss';
 import Nav from './components/Navigation/Nav';
-import Login from './components/Login/Login';
-import Register from './components/Login/Register';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+import Login from './components/Login/Login';
+import Register from './components/Login/Register';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <Router>
-      <div className='app-container'>
-        {/* <Nav /> */}
-        <Switch>
-          <Route path="/news">
-            News
-          </Route>
-          <Route path="/about">
-            About
-          </Route>
-          <Route path="/contact">
-            Contact
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/" exact>
-            Home
-          </Route>
-          <Route path="*">
-            404 Not Found
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div className='app-container'>
+          {/* <Nav /> */}
+          <Switch>
+            <Route path="/news">
+              News
+            </Route>
+            <Route path="/about">
+              About
+            </Route>
+            <Route path="/contact">
+              Contact
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/" exact>
+              Home
+            </Route>
+            <Route path="*">
+              404 Not Found
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+      <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+      />
+    </>
   );
 }
 
--- src/components/Login/Register.js
+++ src/components/Login/Register.js
@@ -1,18 +1,84 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './Register.scss';
 import { useHistory } from "react-router-dom";
+import { toast } from "react-toastify";
 import axios from "axios";
 
 const Register = (props) => {
+    const [soDienThoai, setMobile] = useState("");
+    const [hoTen, setFullname] = useState("");
+    const [email, setEmail] = useState("");
+    const [matKhau, setPassword] = useState("");
+    const [xacNhanMatKhau, setConfirmPassword] = useState("");
+    const defaultValidInput = {
+        isValidMobile: true,
+        isValidEmail: true,
+        isValidPassword: true,
+        isValidConfirmPassword: true
+    };
+    const [objCheckInput, setObjCheckInput] = useState(defaultValidInput);
+
     let history = useHistory();
     const handleLogin = () => {
         history.push("/login");
     }
 
+    const isValidInputs = () => {
+        setObjCheckInput(defaultValidInput);
+
+        if (!soDienThoai) {
+            toast.error("Vui lòng nhập số điện thoại.");
+            setObjCheckInput({ ...defaultValidInput, isValidMobile: false });
+            return false;
+        }
+        let regxMobile = /(0[3|5|7|8|9])+([0-9]{8})\b/g;
+        if (!regxMobile.test(soDienThoai)) {
+            setObjCheckInput({ ...defaultValidInput, isValidMobile: false });
+            toast.error("Vui lòng nhập một số điện thoại hợp lệ!");
+            return false;
+        }
+
+        if (!email) {
+            toast.error("Vui lòng nhập email.");
+            setObjCheckInput({ ...defaultValidInput, isValidEmail: false });
+            return false;
+        }
+        let regxEmail = /\S+@\S+\.\S+/;
+        if (!regxEmail.test(email)) {
+            setObjCheckInput({ ...defaultValidInput, isValidEmail: false });
+            toast.error("Vui lòng nhập một email hợp lệ!");
+            return false;
+        }
+
+        if (!matKhau) {
+            toast.error("Vui lòng nhập mật khẩu.");
+            setObjCheckInput({ ...defaultValidInput, isValidPassword: false });
+            return false;
+        }
+        if (matKhau.length < 8) {
+            toast.error("Mật khẩu phải có ít nhất 8 ký tự.");
+            setObjCheckInput({ ...defaultValidInput, isValidPassword: false });
+            return false;
+        }
+        if (matKhau !== xacNhanMatKhau) {
+            setObjCheckInput({ ...defaultValidInput, isValidConfirmPassword: false });
+            toast.error("Mật khẩu không khớp!");
+            return false;
+        }
+
+        return true;
+    }
+
+    const handleRegister = () => {
+        let check = isValidInputs();
+        let userData = { soDienThoai, hoTen, email, matKhau };
+        console.log(userData);
+    }
+
     useEffect(() => {
-        axios.get("http://localhost:8080/api/test-api").then(data => {
-            console.log(data);
-        })
+        // axios.get("http://localhost:8080/api/test-api").then(data => {
+        //     console.log(data);
+        // })
     }, []);
 
     return (
@@ -27,26 +93,36 @@ const Register = (props) => {
                         <div className="brand d-sm-none">C-Housing</div>
                         <div className="form-group">
                             <label>Số điện thoại:</label>
-                            <input type="text" className="form-control" placeholder="Số điện thoại"/>
+                            <input type="text" className={objCheckInput.isValidMobile ? 'form-control' : 'form-control is-invalid'} placeholder="Số điện thoại"
+                                value={soDienThoai} onChange={(event) => setMobile(event.target.value)}
+                            />
                         </div>               
                         <div className="form-group">
                             <label>Họ và tên:</label>
-                            <input type="text" className="form-control" placeholder="Họ và tên"/>
+                            <input type="text" className="form-control" placeholder="Họ và tên"
+                                value={hoTen} onChange={(event) => setFullname(event.target.value)}
+                            />
                         </div>
                         <div className="form-group">
                             <label>Email:</label>
-                            <input type="text" className="form-control" placeholder="Email"/>
+                            <input type="text" className={objCheckInput.isValidEmail ? 'form-control' : 'form-control is-invalid'} placeholder="Email"
+                                value={email} onChange={(event) => setEmail(event.target.value)}
+                            />
                         </div>
                         <div className="form-group">
                             <label>Mật khẩu:</label>
-                            <input type="password" className="form-control" placeholder="Mật khẩu"/>
+                            <input type="password" className={objCheckInput.isValidPassword ? 'form-control' : 'form-control is-invalid'} placeholder="Mật khẩu"
+                                value={matKhau} onChange={(event) => setPassword(event.target.value)}
+                            />
                         </div>
                         <div className="form-group">
                             <label>Nhập lại mật khẩu:</label>
-                            <input type="password" className="form-control" placeholder="Nhập lại mật khẩu"/>
+                            <input type="password" className={objCheckInput.isValidConfirmPassword ? 'form-control' : 'form-control is-invalid'} placeholder="Nhập lại mật khẩu"
+                                value={xacNhanMatKhau} onChange={(event) => setConfirmPassword(event.target.value)}
+                            />
                         </div>
 
-                        <button className="btn btn-primary">Đăng ký</button>
+                        <button className="btn btn-primary" type="button" onClick={() => handleRegister()}>Đăng ký</button>
 
                         <hr />
                         <div className="text-center">
