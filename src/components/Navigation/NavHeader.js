f176e4a99da98b62ab52415c1751196130dab1cb
Chuong Cao <chuongb2105536@student.ctu.edu.vn>
2025-07-12 12:39:32 +0700

    Hoan thien chuc nang dang xuat

--- src/components/Navigation/NavHeader.js
+++ src/components/Navigation/NavHeader.js
@@ -1,16 +1,33 @@
 import React, { useContext } from "react";
 import './Nav.scss';
-import { NavLink, useLocation } from 'react-router-dom';
+import { Link, NavLink, useLocation, useHistory } from 'react-router-dom';
 import { UserContext } from "../../context/UserContext";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from '../../logo.png';
+import { logoutUser } from '../../services/userService';
+import { toast } from "react-toastify";
 
 const NavHeader = (props) => {
-    const { user } = useContext(UserContext);
+    const { user, logoutContext } = useContext(UserContext);
     const location = useLocation();
+    const history = useHistory();
+
+    const handleLogout = async () => {
+        let data = await logoutUser(); // clear cookies
+        localStorage.removeItem('jwt'); // clear local storage
+        logoutContext(); // clear user in context
+
+        if (data && +data.EC === 0) {
+            toast.success("Đăng xuất thành công!");
+            history.push('/login');
+        } else {
+            toast.error(data.EM);
+        }
+    }
+
     if (user && user.isAuthenticated === true || location.pathname === '/') {
         return (
         <>
@@ -36,14 +53,24 @@ const NavHeader = (props) => {
                                 <NavLink to="/about" className="nav-link">About</NavLink>
                             </Nav>
                             <Nav>
-                                <Nav.Item className="nav-link">
-                                    Xin chào ...!
-                                </Nav.Item>
-                                <NavDropdown title="Cài đặt" id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="#action/3.1">Đổi mật khẩu</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#action/3.2">Đăng xuất</NavDropdown.Item>
-                                </NavDropdown>
+                                {user && user.isAuthenticated === true ?
+                                <>
+                                    <Nav.Item className="nav-link">
+                                        Xin chào {user.account.hoTen}!
+                                    </Nav.Item>
+                                    <NavDropdown title="Cài đặt" id="basic-nav-dropdown">
+                                        <NavDropdown.Item>Đổi mật khẩu</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item>
+                                            <span onClick={() => handleLogout()}>Đăng xuất</span>
+                                        </NavDropdown.Item>
+                                    </NavDropdown>
+                                </>
+                                :
+                                    <Link className="nav-link" to="/login">
+                                        Đăng nhập
+                                    </Link>
+                                }
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
