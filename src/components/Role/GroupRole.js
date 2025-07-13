d0ba09ccce0c975176e91e830de483aa0e1be075
Chuong Cao <chuongb2105536@student.ctu.edu.vn>
2025-07-13 19:27:37 +0700

    Thiet ke giao dien phan quyen

---
+++ src/components/Role/GroupRole.js
@@ -0,0 +1,130 @@
+import './GroupRole.scss';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { fetchGroup } from '../../services/userService';
+import { fetchRole, fetchRolesByGroup } from '../../services/roleService';
+import _ from 'lodash';
+
+const GroupRole = () => {
+    const [userGroups, setUserGroups] = useState([]);
+    const [listRoles, setListRoles] = useState([]);
+    const [selectGroup, setSelectGroup] = useState("");
+    const [assignRoles, setAssignRoles] = useState([]);
+
+    useEffect (() => {
+        getGroups();
+        getAllRoles();
+    }, []);
+
+    const getGroups = async () => {
+        let res = await fetchGroup();
+        if (res && res.EC === 0) {
+            setUserGroups(res.DT);
+        } else {
+            toast.error(res.EM);
+        }
+    }
+
+    const getAllRoles = async () => {
+        let data = await fetchRole();
+        if (data && +data.EC === 0) {
+            setListRoles(data.DT);
+        }
+    }
+
+    const handleOnChangeGroup = async (value) => {
+        setSelectGroup(value);
+        if (value) {
+            let data = await fetchRolesByGroup(value);
+            if (data && +data.EC === 0) {
+                let result = buildDataRolesByGroup(data.DT.Quyens, listRoles);
+                setAssignRoles(result);
+            }
+        }
+    }
+
+    const buildDataRolesByGroup = (groupRoles, allRoles) => {
+        let result = [];
+        if (allRoles && allRoles.length > 0) {
+            allRoles.map(role => {
+                let obj = {};   
+                obj.url = role.url;
+                obj.id = role.id;
+                obj.quyenHan = role.quyenHan;
+                obj.isAssigned = false;
+                if (groupRoles && groupRoles.length > 0) {
+                    obj.isAssigned = groupRoles.some(item => item.url === obj.url);
+                }
+
+                result.push(obj);
+            })
+        }
+        return result;
+    }
+
+    const handleSelectRole = (value) => {
+        const _assignRoles = _.cloneDeep(assignRoles);
+        let foundIndex = _assignRoles.findIndex(item => +item.id === +value);
+        if (foundIndex > -1) {
+            _assignRoles[foundIndex].isAssigned = !_assignRoles[foundIndex].isAssigned;
+        }
+        setAssignRoles(_assignRoles);
+    }
+
+    return (
+        <div className='group-role-container'>
+            <div className='container'>
+                <div className='container mt-3'>
+                    <h3>Nhóm người dùng - Quyền hạn</h3>
+                    <div className='assign'>
+                        <div className='col-12 col-sm-6 form-group'>
+                            <label>Nhóm người dùng (<span className='red'>*</span>):</label>
+                            <select className='form-select'
+                                onChange={(event) => handleOnChangeGroup(event.target.value)}
+                            >
+                                <option value="">-- Chọn nhóm người dùng --</option>
+                                {userGroups.length > 0 &&
+                                    userGroups.map((item, index) => {
+                                        return (
+                                            <option key={`group-${index}`} value={item.id}>{item.tenNhom}</option>
+                                        );
+                                    })
+                                }
+                            </select>
+                        </div>
+                        <hr />
+                        {selectGroup && 
+                            <div className='roles'>
+                                <h4>Phân quyền:</h4>
+                                {assignRoles && assignRoles.length > 0 &&
+                                    assignRoles.map((item, index) => {
+                                        return (
+                                            <div className='form-check' key={`list-role-${index}`}>
+                                                <input
+                                                    className='form-check-input'
+                                                    type='checkbox'
+                                                    value={item.id}
+                                                    id={`list-role-${index}`}
+                                                    checked={item.isAssigned} // isAssigned === true => checked
+                                                    onChange={(event) => handleSelectRole(event.target.value)}
+                                                />
+                                                <label className='form-check-label' htmlFor={`list-role-${index}`}>
+                                                    {item.url}
+                                                </label>
+                                            </div>
+                                        );
+                                    })
+                                }
+                                <div className='mt-3'>
+                                    <button className='btn btn-warning'>Lưu thay đổi</button>
+                                </div>
+                            </div>
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default GroupRole;
--- src/services/roleService.js
+++ src/services/roleService.js
@@ -4,6 +4,10 @@ const createRoles = (roles) => {
     return axios.post('/api/v1/role/create', [...roles]);
 }
 
+const fetchRole = () => {
+    return axios.get('/api/v1/role/read');
+}
+
 const fetchAllRoles = (page, limit) => {
     return axios.get(`/api/v1/role/read?page=${page}&limit=${limit}`);
 }
@@ -16,9 +20,15 @@ const updateCurrentRole = (roleData) => {
     return axios.put('/api/v1/role/update', {...roleData});
 }
 
+const fetchRolesByGroup = (nhomId) => {
+    return axios.get(`/api/v1/role/by-group/${nhomId}`);
+}
+
 export {
     createRoles,
+    fetchRole,
     fetchAllRoles,
     deleteRole,
-    updateCurrentRole
+    updateCurrentRole,
+    fetchRolesByGroup
 };
