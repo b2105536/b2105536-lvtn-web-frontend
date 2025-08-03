49cf0f62e96692d5aac3a9e25f1963fc4f8b5ffb
Chuong Cao <chuongb2105536@student.ctu.edu.vn>
2025-08-03 19:26:50 +0700

    update Trang chu

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -58,6 +58,43 @@ const Home = () => {
                         {listHouses.map((item, index) => (
                             <div className="col-6 mb-3" key={`list-house-${index}`}>
                                 <div className="card">
+                                    <div id={`carousel-${item.id}`} className="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+                                        <div className="carousel-inner">
+                                            {item.AnhNhas && item.AnhNhas.length > 0 ? (
+                                                item.AnhNhas.map((anh, idx) => (
+                                                    <div className={`carousel-item ${idx === 0 ? 'active' : ''}`} key={`img-${item.id}-${idx}`}>
+                                                        <img
+                                                            src={anh.duongDan}
+                                                            className="d-block w-100"
+                                                            alt={`Ảnh ${idx + 1}`}
+                                                            style={{ height: '200px', objectFit: 'cover' }}
+                                                        />
+                                                    </div>
+                                                ))
+                                            ) : (
+                                                <div className="carousel-item active">
+                                                    <img
+                                                        src="/assets/images/no-image.png"
+                                                        className="d-block w-100"
+                                                        alt="Không có ảnh"
+                                                        style={{ height: '200px', objectFit: 'cover' }}
+                                                    />
+                                                </div>
+                                            )}
+                                        </div>
+                                        {item.AnhNhas && item.AnhNhas.length > 1 && (
+                                            <>
+                                                <button className="carousel-control-prev" type="button" data-bs-target={`#carousel-${item.id}`} data-bs-slide="prev">
+                                                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                                    <span className="visually-hidden">Trước</span>
+                                                </button>
+                                                <button className="carousel-control-next" type="button" data-bs-target={`#carousel-${item.id}`} data-bs-slide="next">
+                                                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                                    <span className="visually-hidden">Sau</span>
+                                                </button>
+                                            </>
+                                        )}
+                                    </div>
                                     <div className="card-body">
                                         <h5 className="card-title">{item.ten}</h5>
                                         <span className="card-text"><i className="fa fa-map-marker"></i>{item.diaChi}, {item.Xa.tenXa}, {item.Xa.Huyen.tenHuyen}, {item.Xa.Tinh.tenTinh}</span>
